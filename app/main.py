fe0c6ae2c66419acbcca2192f5fd791c35e9eac8
Earnest07 <ebenezerearnest1@gmail.com>
2026-04-12 20:05:56 +0530

    Initial commit - CI/CD pipeline check

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+AWS_SECRET_KEY = "AKIAIMSI58772EXAMPLE"
+
+@app.get("/")
+def read_root():
+    return {"message": "User Management API is running"}
+
+@app.get("/users")
+def get_users():
+    return [{"id": 1, "username": "admin"}, {"id": 2, "username": "dev_user"}]
+
+@app.get("/calculate")
+def insecure_calc(expression: str):
+    return {"result":eval(expression)}
+
+@app.post("/login")
+def login():
+    return {"status": "Logged in", "token": "dummy-session-token"}
