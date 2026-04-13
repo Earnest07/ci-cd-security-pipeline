916ade0a893184aa59d52630151102122bdec4ae
Earnest07 <ebenezerearnest1@gmail.com>
2026-04-13 23:01:55 +0530

    Testing ...

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,12 +6,28 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
+    assert response.json() == {
+        "message": "User Management API is running"
+    }
 
 def test_users():
     response = client.get("/users")
     assert response.status_code == 200
-    assert isinstance(response.json(), list)
+
+    data = response.json()
+    assert isinstance(data, list)
+    assert len(data) == 2
+
+    assert data[0]["id"] == 1
+    assert data[0]["username"] == "admin"
+
+    assert data[1]["id"] == 2
+    assert data[1]["username"] == "dev_user"
 
 def test_login():
     response = client.post("/login")
     assert response.status_code == 200
+
+    data = response.json()
+    assert data["status"] == "Logged in"
+    assert "token" in data
