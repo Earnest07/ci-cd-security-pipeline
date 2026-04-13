2550f070d0ec36dbddf355c722901a45cbb79d44
Earnest07 <ebenezerearnest1@gmail.com>
2026-04-13 20:19:16 +0530

    Testing gates performance after all 3 gates are ready

--- app/main.py
+++ app/main.py
@@ -1,9 +1,29 @@
 from fastapi import FastAPI
 import os
+import ast
+import operator as op
 
 app = FastAPI()
 
-AWS_SECRET_KEY = "AKIAIMSI58772EXAMPLE"
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY", "")
+
+operators = {
+    ast.Add: op.add,
+    ast.Sub: op.sub,
+    ast.Mult: op.mul,
+    ast.Div: op.truediv,
+}
+
+def safe_eval(expr):
+    def eval_(node):
+        if isinstance(node, ast.Num):
+            return node.n
+        elif isinstance(node, ast.BinOp):
+            return operators[type(node.op)](eval_(node.left), eval_(node.right))
+        else:
+            raise Exception("Unsafe expression")
+
+    return eval_(ast.parse(expr, mode='eval').body)
 
 @app.get("/")
 def read_root():
@@ -14,8 +34,8 @@ def get_users():
     return [{"id": 1, "username": "admin"}, {"id": 2, "username": "dev_user"}]
 
 @app.get("/calculate")
-def insecure_calc(expression: str):
-    return {"result":eval(expression)}
+def secure_calc(expression: str):
+    return {"result": safe_eval(expression)}
 
 @app.post("/login")
 def login():
