87fd648dad32c6fac52f5cf4403f3f4e4701d643
Earnest07 <ebenezerearnest1@gmail.com>
2026-04-13 22:40:32 +0530

    Removed critical issue [ calc() ]

--- app/main.py
+++ app/main.py
@@ -5,24 +5,6 @@ import operator as op
 
 app = FastAPI()
 
-operators = {
-    ast.Add: op.add,
-    ast.Sub: op.sub,
-    ast.Mult: op.mul,
-    ast.Div: op.truediv,
-}
-
-def safe_eval(expr):
-    def eval_(node):
-        if isinstance(node, ast.Num):
-            return node.n
-        elif isinstance(node, ast.BinOp):
-            return operators[type(node.op)](eval_(node.left), eval_(node.right))
-        else:
-            raise Exception("Unsafe expression")
-
-    return eval_(ast.parse(expr, mode='eval').body)
-
 @app.get("/")
 def read_root():
     return {"message": "User Management API is running"}
@@ -30,10 +12,6 @@ def read_root():
 @app.get("/users")
 def get_users():
     return [{"id": 1, "username": "admin"}, {"id": 2, "username": "dev_user"}]
-
-@app.get("/calculate")
-def secure_calc(expression: str):
-    return {"result": safe_eval(expression)}
 
 @app.post("/login")
 def login():
